c455e9360e8f44ac41a8f1f94d6f9889a6d31003
crbnIT <ceaker3@ivytech.edu>
2023-04-16 13:54:25 -0400

    Worked on case study
    did more tinkering with case study problem but can't seem to get the database to work properly. Not sure what's causing it

--- M04/Case_Study_Python_APIs/app.py
+++ M04/Case_Study_Python_APIs/app.py
@@ -1,6 +1,35 @@
 from flask import Flask
+from flask import jsonify
 app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///books.db'
+db = SQLAlchemy(app)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_name = db.Column(db.String(80), unique=True, nullable=False)
+    author = db.Column(db.String(80))
+    publisher = db.Column(db.String(80))
+
+    def __repr__(self):
+        return f"{self.book_name}, {self.author}, {self.publisher}"
 
 @app.route("/")
 def index():
     return "Hello"
+
+@app.route("/books")
+def get_books():
+    books = db.session.query(Book).all()
+    return jsonify([{"book_name": book.book_name, "author": book.author, "publisher": book.publisher} for book in books])
+
+def init_db():
+    with app.app_context():
+        db.create_all()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
